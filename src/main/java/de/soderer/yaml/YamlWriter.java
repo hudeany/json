26476aebcf1ca498e34301fabbe3b11b10dc8a56
Andreas Soderer <git@soderer.de>
2026-01-31 22:53:16 +0100

    Remove debug setting

--- src/main/java/de/soderer/yaml/YamlWriter.java
+++ src/main/java/de/soderer/yaml/YamlWriter.java
@@ -1061,10 +1061,6 @@ public class YamlWriter implements Closeable {
 
 	private YamlWriter write(final String text) throws IOException {
 		outputWriter.write(text);
-
-		// TODO
-		System.out.print(text);
-
 		return this;
 	}
 
